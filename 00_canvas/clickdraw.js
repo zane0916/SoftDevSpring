8ff4566767e1d0d09858da0a7036b8a77702b42c
zane0916 <zwang7@stuy.edu>
2019-01-31 00:31:34 -0500

    hw 00

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,12 +1,27 @@
+console.log("connected");
+
 var newSlate = document.getElementById("clear");
 var board = document.getElementById("slate");
 var tool = document.getElementById("toggle");
 var draw = board.getContext("2d");
+var mode = 1; //if mode = 1, then in RectMode, if -1, then in DotMode
 
-newSlate.addEventListener('click', erase);
+draw.fillStyle = "#0000ff"; //makes blue
+draw.strokeStyle = "#0000ff"; 
 
-erase = draw.clearRect
+newSlate.addEventListener('click', function(e) { draw.clearRect(0,0,600,900) });
 
-drop.addEventListener('click', create);
+tool.addEventListener('click', function(e) { mode = mode * -1 });
 
-create = drop.fillRect(10,10,10,20)
+board.addEventListener('click', color);
+
+var color = function() {
+    if (mode == 1) {
+	draw.fillRect(event.clientX, event.clientY, 10, 20);
+    }
+    else {
+	draw.beginPath();
+	draw.ellipse(event.clientX, event.clientY, 10, 10, 0, 360, false);
+	ctx.stroke();
+    }
+}
